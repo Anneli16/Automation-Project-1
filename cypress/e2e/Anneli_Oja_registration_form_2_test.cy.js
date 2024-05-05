fe67e7deb5bb2a8ec16d1050b4a29a37c01c0851
Anneli16 <annelioja16@gmail.com>
2024-05-05 16:29:47 +0300

    esimesed muutused

--- cypress/e2e/Anneli_Oja_registration_form_2_test.cy.js
+++ cypress/e2e/Anneli_Oja_registration_form_2_test.cy.js
@@ -3,7 +3,7 @@ beforeEach(() => {
 })
 
 /*
-Assignement 4: add content to the following tests
+Assignment 4
 */
 
 describe('Section 1: Functional tests', () => {
@@ -22,7 +22,6 @@ describe('Section 1: Functional tests', () => {
         // Assert that submit button is not enabled
         cy.get('h2').contains('Password').click()
         cy.get('.submit_button').should('not.be.enabled')
-
 
         // Assert that successful message is not visible
         cy.get('#success_message').should('not.be.visible')
@@ -45,7 +44,6 @@ describe('Section 1: Functional tests', () => {
 
         // Add assertion, that error message is not visible anymore
         cy.get('#password_error_message').should('not.be.visible')
-
         
     })
     
@@ -70,6 +68,7 @@ describe('Section 1: Functional tests', () => {
 
         // Assert that after submitting the form system show successful message
         cy.get('#success_message').should('be.visible')
+
     })
 
     it('User can submit form with valid data and only mandatory fields added', ()=>{
@@ -83,7 +82,6 @@ describe('Section 1: Functional tests', () => {
         cy.get('.submit_button').click()
         cy.get('#success_message').should('be.visible')
 
-        
     })
 
     it('Submit button is not enabled when some mandatory field is not present', ()=>{
@@ -105,7 +103,7 @@ describe('Section 1: Functional tests', () => {
 })
 
 /*
-Assignement 5: create more visual tests
+Assignment 5
 */
 
 describe('Section 2: Visual tests', () => {
@@ -115,17 +113,19 @@ describe('Section 2: Visual tests', () => {
         // get element and check its parameter height
         // it should be less than 178 and greater than 100
         cy.get('img').invoke('height').should('be.lessThan', 178)
-            .and('be.greaterThan', 100)   
+            .and('be.greaterThan', 100) 
+
     })
 
-    it('My test for second picture', () => {
+    it.only('My test for second picture', () => {
         // Create similar test for checking the second picture
         cy.log('Will check logo source and size')
         cy.get('img').next().should('have.attr', 'src').should('include', 'cypress_logo')
         // get element and check its parameter height
-        // it should be less than 178 and greater than 100
+        // it should be less than 116 and greater than 85
         cy.get('img').next().invoke('height').should('be.lessThan', 116)
             .and('be.greaterThan', 85)   
+
     });
 
     it('Check navigation part', () => {
@@ -145,6 +145,7 @@ describe('Section 2: Visual tests', () => {
         // Go back to previous page
         cy.go('back')
         cy.log('Back again in registration form 2')
+
     })
 
 
@@ -166,33 +167,11 @@ describe('Section 2: Visual tests', () => {
         // Go back to previous page
         cy.go('back')
         cy.log('Back again in registration form 2')
+
     })
 
-    it('Check that radio button list is correct', () => {
-        // Array of found elements with given selector has 4 elements in total
-        cy.get('input[type="radio"]').should('have.length', 4)
-
-        // Verify labels of the radio buttons
-        cy.get('input[type="radio"]').next().eq(0).should('have.text','HTML')
-        cy.get('input[type="radio"]').next().eq(1).should('have.text','CSS')
-        cy.get('input[type="radio"]').next().eq(2).should('have.text','JavaScript')
-        cy.get('input[type="radio"]').next().eq(3).should('have.text','PHP')
-
-        //Verify default state of radio buttons
-        cy.get('input[type="radio"]').eq(0).should('not.be.checked')
-        cy.get('input[type="radio"]').eq(1).should('not.be.checked')
-        cy.get('input[type="radio"]').eq(2).should('not.be.checked')
-        cy.get('input[type="radio"]').eq(3).should('not.be.checked')
-
-        // Selecting one will remove selection from the other radio button
-        cy.get('input[type="radio"]').eq(0).check().should('be.checked')
-        cy.get('input[type="radio"]').eq(1).check().should('be.checked')
-        cy.get('input[type="radio"]').eq(0).should('not.be.checked')
-    })
-
-    // Create test similar to previous one verifying checkboxes
     it('Check that list of checkboxes is correct', () => {
-        // Array of found elements with given selector has 4 elements in total
+        // Array of found elements with given selector has 3 elements in total
         cy.get('input[class="checkbox vehicles"]').should('have.length', 3)
 
         // Verify labels of the checkboxes
@@ -209,30 +188,8 @@ describe('Section 2: Visual tests', () => {
         cy.get('input[class="checkbox vehicles"]').eq(1).check().should('be.checked')
         cy.get('input[class="checkbox vehicles"]').first().should('be.checked')
         cy.get('input[class="checkbox vehicles"]').eq(2).should('not.be.checked')
+
     })
-
-    it('Car dropdown is correct', () => {
-        // Here is just an example how to explicitely create screenshot from the code
-        // Select second element and create screenshot for this area or full page
-        cy.get('#cars').select(1).screenshot('Cars drop-down')
-        cy.screenshot('Full page screenshot')
-
-        // Here are given different solutions how to get the length of array of elements in Cars dropdown
-        // Next 2 lines of code do exactly the same!
-        cy.get('#cars').children().should('have.length', 4)
-        cy.get('#cars').find('option').should('have.length', 4)
-        
-        // Check that first element in the dropdown has text Volvo
-        cy.get('#cars').find('option').eq(0).should('have.text', 'Volvo')
-        
-        // Advanced level how to check the content of the Cars dropdown
-        cy.get('#cars').find('option').then(options => {
-            const actual = [...options].map(option => option.value)
-            expect(actual).to.deep.eq(['volvo', 'saab', 'opel', 'audi'])
-        })
-    })
-
-    // Create test similar to previous one
 
     it('Favourite animals dropdown is correct', () => {
         // Here is just an example how to explicitely create screenshot from the code
@@ -250,6 +207,7 @@ describe('Section 2: Visual tests', () => {
         cy.get('#animal').find('option').then(options => {
             const actual = [...options].map(option => option.value)
             expect(actual).to.deep.eq(['dog', 'cat', 'snake', 'hippo', 'cow', 'mouse'])
+
         })
     })
 
